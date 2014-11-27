63e79aec2e0dd0af832f8c097bda7f3fbd7b1fa0
Matt Haughey <matt@haughey.com>
2014-11-27 11:54:32 -0800

    updating everything

--- javascripts/sorts.js
+++ javascripts/sorts.js
@@ -1,10 +1,79 @@
 // load jquery
-$(document).ready(function(){
+	$(document).ready(function(){
 
+// random number generator to select a floor for the answer
+	var $answer;
+	$answer = Math.floor(Math.random() * 100) + 1
+
+// make click to display random number and change showbuilding class to display
+	$( ".randomnumber" ).click(function() {
+	$(".randomnumber").html($answer);
+	$(".showbuilding").show();
+});
+
+// change text to represent building levels
+	$(".answer").html($answer);
+
+// make building proper number of floors, which is 20px * $answer
+	var buildingheight = 20 * $answer;
+	$(".building").css("height", buildingheight);
+
+// create shuffled array of length $answer
+for (var a=[],i=0;i<$answer;++i) a[i]=i;
+
+function shuffle(array) {
+  var tmp, current, top = array.length;
+  if(top) while(--top) {
+    current = Math.floor(Math.random() * (top + 1));
+    tmp = array[current];
+    array[current] = array[top];
+    array[top] = tmp;
+  }
+  return array;
+}
+
+a = shuffle(a);
+
+console.log(a)
+
+
+// bubble sort the array to count comparisons/swaps
+
+var sort = function (list) {
+ 
+    var comparisons = 0,
+        swaps = 0,
+        endIndex = 0,
+        len = list.length - 1;
+ 
+    for (var i = 0; i < len; i++) {
+ 
+        for (var j = 0, swapping, endIndex = len - i; j < endIndex; j++) {
+            comparisons++;
+ 
+            if (list[j] > list[j + 1]) {
+         
+                swapping = list[j];
+ 
+                list[j] = list[j + 1];
+                list[j + 1] = swapping;
+ 
+                swaps++;
+            };
+        };
+    }
+ 
+    console.log("--Bubble Sort--");
+    console.log("Comparisons: " + comparisons);
+    console.log("Swaps: " + swaps);
+                 
+    return list;
+
+};
+
+// bubble sort the randomized array
+sort(a);
 
 });
 
-// random number generator to select a floor for the answer
 
-var answer;
-answer = Math.floor(Math.random() * 100) + 1
