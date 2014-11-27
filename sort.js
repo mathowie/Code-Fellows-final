535f1548048498686c93541707271b2f35da047f
mathowie <matt@haughey.com>
2014-11-26 20:43:17 -0800

    updated the main page, started js

---
+++ sort.js
@@ -0,0 +1,21 @@
+// create 1-100 number generator
+
+var answer;
+answer = Math.floor(Math.random() * 100) + 1
+
+// create variable of the correct guess 1-100
+
+document.write(answer);
+
+// draw css rectangle with # of lines showing correct answer
+
+// create a 0-100 array
+
+// sort array to find the right answer in two ways
+// sort array via bubble sort (inefficent)
+// sort array using most efficient sort (quicksort)
+
+// report number of steps to find solution in bubblesort
+
+// report number of steps to find solution in quicksort
+
